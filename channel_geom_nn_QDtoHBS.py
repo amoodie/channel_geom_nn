7aeb91e1f446b2aa2c68bd28996709af0ff6a225
amoodie <amoodie@rice.edu>
2020-02-16 15:45:21 -0600

    rework data into a class

--- channel_geom_nn_QDtoHBS.py
+++ channel_geom_nn_QDtoHBS.py
@@ -5,65 +5,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 import tensorflow as tf
 
+import dataset
 import plot_utils
+
 
 tf.reset_default_graph()
 
-# read the data
-# df = pd.read_csv('data/mcelroy_dataclean.csv') # read data set using pandas
-# df = pd.read_csv('data/wilkerson_dataclean.csv') # read data set using pandas
-df = pd.read_csv('data/ShieldsJHRData.csv') # read data set using pandas
-df = df.dropna(inplace = False)  # Remove all nan entries.
 
-print('Data summary:\n')
-print(df.describe(), '\n\n') # Overview of dataset
-
-# subset for train and test and rescale all values
-df_train, df_test = train_test_split(df, test_size=0.30)
-
-# we want to predict the H, B, and S given Qbf, D50
-# this only works because HBS are highly correlated.
-# y is output and x is input features
-
-# do some normalization
-scaler = MinMaxScaler() # For normalizing dataset
-
-# min max normalization
-# X_train = scaler.fit_transform(df_train.drop(['Bbf.m', 'Hbf.m', 'S'], axis=1).values)
-# y_train = scaler.fit_transform(df_train[['Bbf.m', 'Hbf.m', 'S']].values)
-# X_test = scaler.fit_transform(df_test.drop(['Bbf.m', 'Hbf.m', 'S'], axis=1).values)
-# y_test = scaler.fit_transform(df_test[['Bbf.m', 'Hbf.m', 'S']].values)
-
-# min max log(x) normalization
-# X_train = scaler.fit_transform(np.log10(df_train.drop(['Bbf.m', 'Hbf.m', 'S'], axis=1).values))
-# y_train = scaler.fit_transform(np.log10(df_train[['Bbf.m', 'Hbf.m', 'S']].values))
-# X_test = scaler.fit_transform(np.log10(df_test.drop(['Bbf.m', 'Hbf.m', 'S'], axis=1).values))
-# y_test = scaler.fit_transform(np.log10(df_test[['Bbf.m', 'Hbf.m', 'S']].values))
-# logged = True
-# normed = True
-
-# log(x) normalization
-X_train = (np.log10(df_train.drop(['Bbf.m', 'Hbf.m', 'S'], axis=1).values))
-y_train = (np.log10(df_train[['Bbf.m', 'Hbf.m', 'S']].values))
-X_test = (np.log10(df_test.drop(['Bbf.m', 'Hbf.m', 'S'], axis=1).values))
-y_test = (np.log10(df_test[['Bbf.m', 'Hbf.m', 'S']].values))
-logged = True
-normed = False
-
-# no normalization (be sure to turn off below for plotting)
-# X_train = (df_train.drop(['Bbf.m', 'Hbf.m', 'S'], axis=1).values)
-# y_train = (df_train[['Bbf.m', 'Hbf.m', 'S']].values)
-# X_test = (df_test.drop(['Bbf.m', 'Hbf.m', 'S'], axis=1).values)
-# y_test = (df_test[['Bbf.m', 'Hbf.m', 'S']].values)
-# logged = False
-# normed = False
-
-# set up data for mini-batching during training
-batch_size = 1
-buffer_size = 15
-ds_train = tf.data.Dataset.from_tensor_slices((X_train, y_train)).repeat().batch(batch_size).shuffle(buffer_size)
-it_train = ds_train.make_one_shot_iterator()
-xs, ys = it_train.get_next()
+data = dataset.ChannelDataset(datasrc='trampush', scale='log')
 
 
 def denormalize(df, norm_data):
@@ -120,9 +69,11 @@ def nn_model(X_data, input_dim):
 
     return output, W_O
 
+# get an initial dataset
+xs, ys = data.get_next()
 
 # the model
-output, W_O = nn_model(xs, X_train.shape[1])
+output, W_O = nn_model(xs, data.X_train.shape[1])
 
 # mean squared error cost function
 # loss = tf.reduce_sum(tf.square(output - ys))
@@ -138,12 +89,15 @@ train = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
 _loss_summary = tf.summary.scalar(name='loss summary', tensor=loss)
 # correct_pred = tf.argmax(output, 1)
 # accuracy = tf.losses.mean_squared_error(tf.cast(correct_pred, tf.float32), ys)
-# saver = tf.train.Saver()
+saver = tf.train.Saver()
 
 c_train = []
 c_test = []
 
-save_training = False
+save_training = True
+# intrain_y_test = data.y_scaler.inverse_transform(y_test)
+# intrain_y_train = data.y_scaler.inverse_transform(data.y_train)
+
 with tf.Session() as sess:
     # Initiate session and initialize all vaiables
     sess.run(tf.global_variables_initializer())
@@ -152,116 +106,78 @@ with tf.Session() as sess:
 
     it = 0
     n_epoch = 10
-    n_batch_per_epoch = int( np.floor(X_train.shape[0] / batch_size) )
+    
     for i in range(n_epoch):
-        ds_train.shuffle(buffer_size)
-        for j in range(n_batch_per_epoch):
+        
+        data.shuffle()
+        
+        for j in range(data.batches_per_epoch):
             # Run loss and train with each batch
             sess.run([loss, train])
 
-            c_train.append(sess.run(loss, feed_dict = {xs:X_train, ys:y_train}))
-            c_test.append(sess.run(loss, feed_dict = {xs:X_test, ys:y_test}))
+            c_train.append(sess.run(loss, feed_dict = {xs:data.X_train, ys:data.y_train}))
+            c_test.append(sess.run(loss, feed_dict = {xs:data.X_test, ys:data.y_test}))
 
             loss_summary = sess.run(_loss_summary)
             writer.add_summary(loss_summary, it)
 
             if save_training:
                 
+                pred_test = sess.run(output, feed_dict={xs:data.X_test})
+                pred_train = sess.run(output, feed_dict={xs:data.X_train})
+                pred_test_orig = data.y_scaler.inverse_transform(pred_test)
+                pred_train_orig = data.y_scaler.inverse_transform(pred_train)
 
-                intrain_pred_test = sess.run(output, feed_dict={xs:X_test})
-                intrain_pred_train = sess.run(output, feed_dict={xs:X_train})
-
-                intrain_y_test = denormalize(df_test, y_test)
-                intrain_pred_test = denormalize(df_test, intrain_pred_test)
-                intrain_y_train = denormalize(df_train, y_train)
-                intrain_pred_train = denormalize(df_train, intrain_pred_train)
-                figN = plot_utils.compare_plot(df, df_train, df_test, intrain_pred_train, intrain_pred_test)
+                figN = plot_utils.compare_plot(data.df, data.y_train_orig, data.y_test_orig, 
+                                               pred_train_orig, pred_test_orig)
                 figN.savefig('figures/training/{:04d}.png'.format(it))
                 plt.close(figN)
-                print('Epoch:', i, ', train loss:', c_train[i*n_batch_per_epoch], ', test loss:', c_test[i*n_batch_per_epoch])
+                
 
             it += 1
         
-        print('Epoch:', i, ', train loss:', c_train[i*n_batch_per_epoch], ', test loss:', c_test[i*n_batch_per_epoch])
+        print('Epoch:', i, ', train loss:', c_train[-1], ', test loss:', c_test[-1])
 
     # finished training
     print('\nTraining complete.')
     print('Total iterations: ', it)
-    print('test loss :', sess.run(loss, feed_dict={xs:X_test, ys:y_test}), '\n')
+    print('test loss :', sess.run(loss, feed_dict={xs:data.X_test, ys:data.y_test}), '\n')
     writer.close()
 
     # save the model
-    # save_path = saver.save(sess, "log/channel_geom_nn_QDtoHBS.ckpt")
+    save_path = saver.save(sess, "log/channel_geom_nn_QDtoHBS.ckpt")
 
     # predict output of test data after training
-    pred_test = sess.run(output, feed_dict={xs:X_test})
-    pred_train = sess.run(output, feed_dict={xs:X_train})
-
-    # predict for some range
-    qlist = np.array([2000, 3000])
-    dlist = np.linspace(1, 20, num=19) # np.array([1.0, 5.0, 10, 100])
-    bhs = np.empty((qlist.shape[0]*dlist.shape[0], 3))
-    dep = 0
-    for q in iter(qlist):
-        for d in iter(dlist):
-            invect = np.log10( np.array([q, d]).reshape(-1, 2) )
-            pred_rng = sess.run(output, feed_dict={xs:invect})
-            pred_dn = denormalize(df_test, pred_rng)
-            bhs[dep, :] = pred_dn
-            dep += 1
-    bhs = bhs.reshape((dlist.shape[0], 3, -1))   
-
-# denormalize data
-y_test = denormalize(df_test, y_test)
-pred_test = denormalize(df_test, pred_test)
-y_train = denormalize(df_train, y_train)
-pred_train = denormalize(df_train, pred_train)
-
+    pred_test = sess.run(output, feed_dict={xs:data.X_test})
+    pred_train = sess.run(output, feed_dict={xs:data.X_train})
+    pred_test_orig = data.y_scaler.inverse_transform(pred_test)
+    pred_train_orig = data.y_scaler.inverse_transform(pred_train)
 
 # plots
 fig1, axes1 = plt.subplots(nrows=1, ncols=2, figsize=(6,4))
-axes1[0].hist([df_train['Qbf.m3s'], df_test['Qbf.m3s']], histtype = 'bar', density = True)
+axes1[0].hist([data.df_train['Qbf.m3s'], data.df_test['Qbf.m3s']], histtype = 'bar', density = True)
 axes1[0].set_xlabel('Qbf (m3/s)')
-axes1[1].hist([df_train['D50.mm'], df_test['D50.mm']], histtype = 'bar', density = True)
+axes1[1].hist([data.df_train['D50.mm'], data.df_test['D50.mm']], histtype = 'bar', density = True)
 axes1[1].set_xlabel('D50 (mm)')
 plt.legend(['train', 'test'], loc = 'best')
 fig1.savefig('figures/split.png')
 
-fig2 = plot_utils.compare_plot(df, df_train, df_test, pred_train, pred_test)
+fig2 = plot_utils.compare_plot(data.df,  data.y_train_orig, data.y_test_orig, 
+                               pred_train_orig, pred_test_orig)
 fig2.savefig('figures/compare.png')
 
 fig3, ax3 = plt.subplots(figsize=(6,4))
-ax3.plot(np.arange(len(c_train)) / n_batch_per_epoch, np.array(c_train))
-ax3.plot(np.arange(len(c_test)) / n_batch_per_epoch, np.array(c_test))
+ax3.plot(np.arange(len(c_train)) / data.batches_per_epoch, np.array(c_train))
+ax3.plot(np.arange(len(c_test)) / data.batches_per_epoch, np.array(c_test))
 ax3.set_xlabel('epoch')
 ax3.set_ylabel('loss')
 plt.legend(['train', 'test'], loc = 'best')
 fig3.savefig('figures/train.png')
 
 fig4, ax4 = plt.subplots(figsize=(8,6))
-pd.plotting.scatter_matrix(np.log10(df), ax=ax4)
+pd.plotting.scatter_matrix(np.log10(data.df), ax=ax4)
 fig4.savefig('figures/scatter.png')
 
 fig5, ax5 = plt.subplots()
-ax5.matshow(np.log10(df).corr())
+ax5.matshow(np.log10(data.df.corr()))
 fig5.savefig('figures/corr_mat.png')
-
-fig6, ax6 = plt.subplots(nrows=3, ncols=1, figsize=(6,10))
-ax6[0].scatter(df['D50.mm'], df['Hbf.m'])
-for p in np.arange(bhs.shape[2]):
-    ax6[0].plot(dlist, bhs[:, 1, p])
-    ax6[0].set_ylabel('depth (m)')
-ax6[1].scatter(df['D50.mm'], df['Bbf.m'])
-for p in np.arange(bhs.shape[2]):
-    ax6[1].plot(dlist, bhs[:, 0, p])
-    ax6[1].set_ylabel('width (m)')
-ax6[2].scatter(df['D50.mm'], df['S'])
-for p in np.arange(bhs.shape[2]):
-    ax6[2].plot(dlist, bhs[:, 2, p])
-    ax6[2].set_ylabel('slope (1)')
-ax6[2].set_yscale('log')
-# ax6[0].set_xscale('log')
-ax6[0].legend(qlist)
-# plt.show()
-
-fig6.savefig('figures/input_test.png')
--- plot_utils.py
+++ plot_utils.py
@@ -2,10 +2,14 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-def compare_plot(df, df_train, df_test, pred_train=None, pred_test=None):
+def compare_plot(df, y_train, y_test, pred_train, pred_test):
+    """
+    Plot comparison of the predictions and actual values (1:1 plots).
+    """
     fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(9,4))
-    ax[0].plot(df_train['Hbf.m'], pred_train[:,1], 'o', alpha=0.2)
-    ax[0].plot(df_test['Hbf.m'], pred_test[:,1], 'o', alpha=0.8)
+
+    ax[0].plot(y_train[:,1], pred_train[:,1], 'o', alpha=0.2)
+    ax[0].plot(y_test[:,1], pred_test[:,1], 'o', alpha=0.8)
     ax[0].plot([df['Hbf.m'].min()/10, df['Hbf.m'].max()*10], [df['Hbf.m'].min()/10, df['Hbf.m'].max()*10], 'k-', lw=2)
     ax[0].axis('square')
     ax[0].set_title('depth H, (m)')
@@ -17,8 +21,8 @@ def compare_plot(df, df_train, df_test, pred_train=None, pred_test=None):
     ax[0].set_ylim([df['Hbf.m'].min()/10, df['Hbf.m'].max()*10])
     ax[0].legend(['train', 'test'], loc = 'upper left')
 
-    ax[1].plot(df_train['Bbf.m'], pred_train[:,0], 'o', alpha=0.2)
-    ax[1].plot(df_test['Bbf.m'], pred_test[:,0], 'o', alpha=0.8)
+    ax[1].plot(y_train[:,0], pred_train[:,0], 'o', alpha=0.2)
+    ax[1].plot(y_test[:,0], pred_test[:,0], 'o', alpha=0.8)
     ax[1].plot([df['Bbf.m'].min()/10, df['Bbf.m'].max()*10], [df['Bbf.m'].min()/10, df['Bbf.m'].max()*10], 'k-', lw=2)
     ax[1].axis('square')
     ax[1].set_title('width B, (m)')
@@ -28,8 +32,8 @@ def compare_plot(df, df_train, df_test, pred_train=None, pred_test=None):
     ax[1].set_xlim([df['Bbf.m'].min()/10, df['Bbf.m'].max()*10])
     ax[1].set_ylim([df['Bbf.m'].min()/10, df['Bbf.m'].max()*10])
 
-    ax[2].plot(df_train['S'], pred_train[:,2], 'o', alpha=0.2)
-    ax[2].plot(df_test['S'], pred_test[:,2], 'o', alpha=0.8)
+    ax[2].plot(y_train[:,2], pred_train[:,2], 'o', alpha=0.2)
+    ax[2].plot(y_test[:,2], pred_test[:,2], 'o', alpha=0.8)
     ax[2].plot([df['S'].min()/10, df['S'].max()*10], [df['S'].min()/10, df['S'].max()*10], 'k-', lw=2)
     ax[2].axis('square')
     ax[2].set_title('slope S, (1)')
@@ -39,4 +43,5 @@ def compare_plot(df, df_train, df_test, pred_train=None, pred_test=None):
     ax[2].set_xlim([df['S'].min()/10, df['S'].max()*10])
     ax[2].set_ylim([df['S'].min()/10, df['S'].max()*10])
 
+    plt.tight_layout()
     return fig
